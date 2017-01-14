18f302fbdf495d59dd8053fb1dba46eb165e99ae
winster <wtjose@gmail.com>
2017-01-15 00:28:41 +0530

    waiting for order

--- smartjekhome/app.js
+++ smartjekhome/app.js
@@ -66,7 +66,7 @@ button.watch(function(err, value){
      lcd.clear();
      lcd.setCursor(0,0);
      var service = selectedServices[COUNTER];
-     lcd.print('Made order for', function(err){
+     lcd.print('Made order for ', function(err){
        if(err){
          console.log('error in printing ', err);
        }
@@ -75,12 +75,12 @@ button.watch(function(err, value){
            lcd.setCursor(0,0);
            lcd.print('Waiting for vend', function(err){
            });
-           lcd.setCursor(0,2);
+           lcd.setCursor(0,1);
            lcd.print('or to receive', function(err){
            });
        }, 5000);
      });
-     lcd.setCursor(0,2);
+     lcd.setCursor(0,1);
      lcd.print(service, function(err){
      });
   }
